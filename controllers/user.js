0fab02b27fd81d7452afa270e5210b08ff6a9484
michael shamah <shamah.michael@gmail.com>
2016-06-30 14:48:57 -0400

    spotfiy api is working

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,24 @@
 
 const router = require('express').Router();
 const songsService = require('../models/spotify')
-router.get('/', songsService.artist, function(req, res){
+router.get('/artists', songsService.artist, function(req, res){
   var artist= JSON.parse(res.artist.body)
   var names=artist.artists.items
-  console.log(names)
-  res.render('user', {names: names})
+  res.render('artists', {names: names})
 });
+
+router.get('/songs', songsService.song, function(req, res){
+  var songs =JSON.parse(res.song.body)
+  songs=songs.tracks.items
+  res.render('songs', {songs: songs})
+})
+
+router.get('/albums', songsService.album, function(req, res){
+  console.log(res.record)
+  var albums =JSON.parse(res.record.body)
+  albums =albums.albums.items
+  res.render('albums', {albums: albums})
+})
+
 
 module.exports= router
--- models/spotify.js
+++ models/spotify.js
@@ -1,12 +1,28 @@
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 var request = require('request');
-const dbConnection = 'mongodb://localhost:27017/spotify';
+// const dbConnection = 'mongodb://localhost:27017/spotify';
 // const key= " Bearer BQD5f6pVLf6P070XlmIs_XyfCYQE9y8qe3IWrZohUdGrrO8bkv94yAyZJI12BHo9bbPjLwXA6QImBwJf3fDvOtcBU0edVUjo5sz2zYQbibKY8Kh4ukWyqUg01WdKI31c3Vc2VBBHcAYSyuyJrkCtULcSdAkcxyPmOQZAQp6lMfEpI90rduyro9-Oqkb8ZMlZKenAmWk7C94Sjgy58bU5p7QWZO6Gx-OL4PICZItTxpVqRYum9rTncCnjdzSfAO_5BERwg_-Q5-T8Aob66ge7qOD98A0KW1pCTF0A91LzSw2FRFAMgGBb10QY"
 
 function artist(req, res, next){
   request("https://api.spotify.com/v1/search?q=script+&type=artist", function (error, artist) {
-res.artist= artist
-  next()
+    res.artist= artist
+    next()
   })
 }
- module.exports= {artist}
+
+function song(req, res, next){
+  request("https://api.spotify.com/v1/search?q=work+&type=track", function (error, song) {
+    res.song= song
+    next()
+  })
+}
+
+function album(req, res, next){
+  request("https://api.spotify.com/v1/search?q=no+sound+without+silence&type=album", function (error, record) {
+    res.record= record
+    next()
+  })
+}
+ module.exports= {artist, song, album}
+
+
