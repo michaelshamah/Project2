f2d7902a2968609d96a069aed3ddd470a60f43f2
michael shamah <shamah.michael@gmail.com>
2016-07-05 10:00:52 -0400

    stylings

--- controllers/user.js
+++ controllers/user.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const { createUser, loginUser } = require('../models/user');
 
 router.get('/new', function(req,res) {
-  res.render('/user/new', {user: req.session.user});
+  res.render('/new', {user: req.session.user});
 });
 
 router.post('/new', createUser, function(req,res) {
   console.log(req.body);
-  res.redirect('/login', {user: req.session.user});
+  res.redirect('login', {user: req.session.user});
 });
 
 router.get('/login', function(req,res) {
-  res.render('/user/login', {user: req.session.user});
+  res.render('user/login', {user: req.session.user});
 });
 
 router.post('/login', loginUser, function(req,res) {
